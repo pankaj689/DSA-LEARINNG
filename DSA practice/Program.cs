70788566824ad872408bbc58a5198482662ab8a1
pankaj.patil <pankaj.patil@L-MHP-PANKAJP>
2025-05-05 13:02:13 +0530

    Find the duplicate number in an array of integers.

--- DSA practice/Program.cs
+++ DSA practice/Program.cs
@@ -1,2 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using DSA_practice;
+
 Console.WriteLine("Hello, World!");
+
+
+
+coadfunction coadfunction = new coadfunction();
+
+List<int> arguments = new List<int>() { 1,1,1,1,2,2,3,4,5,6,7};
+
+//Find the duplicate number in an array of integers.
+
+List<int> outpi = coadfunction.dublicatearray(arguments);
+foreach(int i in outpi)
+{
+  //  Console.Write(i);
+
+    Console.WriteLine(i);
+}
+
+
+Console.ReadLine();
---
+++ DSA practice/coadfunction.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA_practice
+{
+    internal class coadfunction
+    {
+
+        //Find the duplicate number in an array of integers.
+
+        public List<int> dublicatearray(List<int> inputarray)
+        {
+
+            List<int> result = new List<int>();
+
+            Dictionary<int, int> numberdict = new Dictionary<int, int>();
+
+
+            foreach(int i in inputarray)
+            {
+                if (numberdict.ContainsKey(i))
+                {
+                    numberdict[i] += 1;
+
+                    if(numberdict[i] == 2)
+                    {
+                        result.Add(i);
+                    }
+                   
+                }
+                else
+                {
+                    numberdict[i] = 1;
+                }
+            }
+
+
+            return result;
+        }
+    }
+}
